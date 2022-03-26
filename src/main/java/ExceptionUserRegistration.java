033331a346d6d4836f3bd6133cb92a528b001b77
veeresh <veereshyalawar2@gmail.com>
2022-03-26 16:18:00 +0530

    added uc6 file

--- src/main/java/ExceptionUserRegistration.java
+++ src/main/java/ExceptionUserRegistration.java
@@ -5,13 +5,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * As a User need to follow predefined Password rules. Rule1– minimum 8 Characters
+ * UC1- As a User need to enter a valid First Name - First name starts with Cap
+ *      and has minimum 3 characters 
+ * UC2- As a User need to enter a valid Last Name-Last name starts with Cap and
+ *      has minimum 3 characters 
+ * UC3- As a User need to enter a valid email 
+ * UC4- As a User need to follow 
+ *      predefined Mobile Format Country code follow by space and 10 digit number 
+ * UC5- As a User need to follow pre-defined Password rules. Rule1– minimum 8 Characters
+ * UC6- Rule2 – Should have at least 1 Upper Case
  * 
  * @author user-Veeresh
  *
  */
 public class ExceptionUserRegistration {
 
+	
 
 	/**
 	 * created method PasswordRule1 and passing parameter in this it will check
@@ -21,9 +30,22 @@ public class ExceptionUserRegistration {
 	 * @param password- passing password
 	 * @return -return to method created
 	 */
-
 	public boolean passwordRule1(String str) {
 		Pattern pattern = Pattern.compile("[a-zA-Z]{8,}");
+		Matcher matcher = pattern.matcher(str);
+		return matcher.matches();
+	}
+
+	/**
+	 * created method PasswordRule2 and passing parameter in this it will check
+	 * Password is valid or not result will be in true or false because method is
+	 * boolean type
+	 * 
+	 * @param password- passing password
+	 * @return -return to method created
+	 */
+	public boolean passwordRule2(String str) {
+		Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z]).{8,}$");
 		Matcher matcher = pattern.matcher(str);
 		return matcher.matches();
 	}
--- src/test/java/ExceptionUserRegistrationTest.java
+++ src/test/java/ExceptionUserRegistrationTest.java
@@ -1,18 +1,20 @@
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ExceptionUserRegistrationTest {
 	ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
 
-
-
+	/**
+	 * test case for rule1 and rule2
+	 */
+	
 	@Test
 	/**
-	 * @author veeresh
 	 * created method givePasswordRule1_IsProper_ReturnTrue() for true condition
 	 */
 	public void givenPasswordRule1_IsProper_ReturnTrue() {
-		boolean actualResult = exceptionUserRegistration.passwordRule1("aBcdeFghiM");
+		boolean actualResult = exceptionUserRegistration.passwordRule1("abcdefghi");
 		Assert.assertEquals(true, actualResult);
 	}
 
@@ -22,7 +24,26 @@ public class ExceptionUserRegistrationTest {
 	 * condition
 	 */
 	public void givenPasswordRule1_IsNotProper_ReturnFalse() {
-		boolean actualResult = exceptionUserRegistration.passwordRule1("abCd");
+		boolean actualResult = exceptionUserRegistration.passwordRule1("abcd");
+		Assert.assertEquals(false, actualResult);
+	}
+
+	@Test
+	/**
+	 * created method givePasswordRule2_IsProper_ReturnTrue() for true condition
+	 */
+	public void givenPasswordRule2_IsProper_ReturnTrue() {
+		boolean actualResult = exceptionUserRegistration.passwordRule2("qwgthRLjkdl");
+		Assert.assertEquals(true, actualResult);
+	}
+
+	@Test
+	/**
+	 * created method givePasswordRule2_IsNotProper_ReturnFalse() for false
+	 * condition
+	 */
+	public void givenPasswordRule2_IsNotProper_ReturnFalse() {
+		boolean actualResult = exceptionUserRegistration.passwordRule2("jnsdjcvcp");
 		Assert.assertEquals(false, actualResult);
 	}
 }
